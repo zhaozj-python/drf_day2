c52f58aa8b802580ad2e45d97d9e81dc01100d76
zhaozj-python <1351628463@qq.com>
2020-11-01 20:04:16 +0800

    添加genericAPIView

--- drf_teacher_day2/teacher_app/urls.py
+++ drf_teacher_day2/teacher_app/urls.py
@@ -5,4 +5,6 @@ from teacher_app import views
 urlpatterns = [
     path("teacher/", views.TeacherAPIView.as_view()),
     path("teacher/<str:id>/", views.TeacherAPIView.as_view()),
+    path("teachergen/", views.TeacherGenericAPIView.as_view()),
+    path("teachergen/<str:id>/", views.TeacherGenericAPIView.as_view()),
 ]
--- drf_teacher_day2/teacher_app/views.py
+++ drf_teacher_day2/teacher_app/views.py
@@ -1,6 +1,10 @@
 from rest_framework.response import Response
 
 from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
+from rest_framework import mixins
+from rest_framework import generics
+from rest_framework import viewsets
 # Create your views here.
 from teacher_app.models import Teacher
 from teacher_app.serializers import TeacherSerializer, TeacherDeSerializer, TeacherModelSerializer
@@ -132,3 +136,31 @@ class TeacherAPIView(APIView):
             })
 
 
+class TeacherGenericAPIView(GenericAPIView,
+                         mixins.ListModelMixin,
+                         mixins.RetrieveModelMixin,
+                         mixins.DestroyModelMixin,
+                         mixins.CreateModelMixin,
+                         mixins.UpdateModelMixin):
+    queryset = Teacher.objects.filter()
+    serializer_class = TeacherModelSerializer
+
+    lookup_field = "id"
+
+    def get(self, request, *args, **kwargs):
+        if "id" in kwargs:
+            return self.retrieve(request, *args, **kwargs)
+
+        return self.list(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    # def patch(self, request, *args, **kwargs):
+    #     return self.
